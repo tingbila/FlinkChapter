0f5bbbbf3e2106acbbcb2f6ab9f902326e013e96
ajjsk <ajjsk@qq.com>
2023-04-02 09:39:17 +0800

    增量聚合

---
+++ src/main/java/chapter06/Window_IncreReduce.java
@@ -0,0 +1,55 @@
+package chapter06;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+public class Window_IncreReduce {
+    public static void main(String[] args) throws Exception {
+        // get the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
+
+        // get input data by connecting to the socket
+        DataStream<String> text = env.socketTextStream("192.168.40.101", 9999, "\n");
+
+        // parse the data, group it, window it, and aggregate the counts
+        KeyedStream<Tuple2<String, Integer>, String> keyedStream = text.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                // normalize and split the line
+                String[] tokens = value.toLowerCase().split("\\W+");
+
+                // emit the pairs
+                for (String token : tokens) {
+                    if (token.length() > 0) {
+                        out.collect(new Tuple2<>(token, 1));
+                    }
+                }
+            }
+        }).keyBy(value -> value.f0);
+
+        WindowedStream<Tuple2<String, Integer>, String, TimeWindow> dataWS = keyedStream.timeWindow(Time.seconds(10));
+
+        DataStream<Tuple2<String, Integer>> reduceDs = dataWS.reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> reduce(Tuple2<String, Integer> value1, Tuple2<String, Integer> value2) throws Exception {
+                //每来一条数据就进行一次叠加计算,但是最终结果只有在窗口触发时才会最终输出
+                System.out.println(value1 + "<---->" + value2);
+                return Tuple2.of(value1.f0, value1.f1 + value2.f1);
+            }
+        });
+        reduceDs.print("------------->聚合结果:----------->");
+
+        env.execute("Window_IncreReduce");
+    }
+}
