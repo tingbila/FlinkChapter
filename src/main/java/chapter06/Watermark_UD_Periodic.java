d92db1ad01304925cbd84a0f5e565c5f2f0917dd
ajjsk <ajjsk@qq.com>
2023-04-02 15:54:16 +0800

    增量聚合

---
+++ src/main/java/chapter06/Watermark_UD_Periodic.java
@@ -0,0 +1,79 @@
+package chapter06;
+
+import bean.WaterSensor;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.RestOptions;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import javax.annotation.Nullable;
+
+public class Watermark_UD_Periodic {
+    public static void main(String[] args) throws Exception {
+        // get the execution environment
+        Configuration conf = new Configuration();
+        conf.set(RestOptions.PORT, 8083);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
+        env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        // get input data by connecting to the socket
+        DataStream<String> dataStreamSource = env.socketTextStream("192.168.40.101", 9999, "\n");
+        DataStream<WaterSensor> mapDataStream = dataStreamSource.map(new RichMapFunction<String, WaterSensor>() {
+            @Override
+            public WaterSensor map(String line) throws Exception {
+                String[] spited = line.split(",");
+                return new WaterSensor(spited[0], Long.valueOf(spited[1]), Integer.valueOf(spited[2]));
+            }
+        }).assignTimestampsAndWatermarks(new AssignerWithPeriodicWatermarks<WaterSensor>() {
+            //自己实现的话需要保证watermark不要回退
+            //代码仿照BoundedOutOfOrdernessTimestampExtractor的源码编写即可
+            private long maxOutOfOrderness = 2 * 1000L;   //自定义乱序时间
+            private long currentMaxTimestamp = Long.MIN_VALUE;
+            private long lastEmittedWatermark = Long.MIN_VALUE;
+
+            @Nullable
+            @Override
+            public Watermark getCurrentWatermark() {
+                System.out.println("Periodic...Generate...Watermark");
+                // this guarantees that the watermark never goes backwards.
+                long potentialWM = currentMaxTimestamp - maxOutOfOrderness;
+                if (potentialWM >= lastEmittedWatermark) {
+                    lastEmittedWatermark = potentialWM;
+                }
+                return new Watermark(lastEmittedWatermark);
+            }
+
+            @Override
+            public long extractTimestamp(WaterSensor element, long recordTimestamp) {
+                long timestamp = element.getTs() * 1000L;
+                if (timestamp > currentMaxTimestamp) {
+                    currentMaxTimestamp = timestamp;
+                }
+                return timestamp;
+            }
+        });
+
+        mapDataStream.print();
+        DataStream<Long> processData = mapDataStream.keyBy(value -> value.getId()).timeWindow(Time.seconds(5))  //[0,5)本应该在event_time >= 5秒时触发计算
+                .process(new ProcessWindowFunction<WaterSensor, Long, String, TimeWindow>() {
+                    @Override
+                    public void process(String key, Context context, Iterable<WaterSensor> elements, Collector<Long> out) throws Exception {
+                        out.collect(elements.spliterator().estimateSize());
+                    }
+                });
+
+        processData.print();
+
+        env.execute("Watermark_UD_Periodic");
+    }
+}
