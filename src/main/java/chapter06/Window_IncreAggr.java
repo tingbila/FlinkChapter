0f62ffe929c7e6b0c882720bef0e98c987655546
ajjsk <ajjsk@qq.com>
2023-04-02 11:45:23 +0800

    增量聚合

---
+++ src/main/java/chapter06/Window_IncreAggr.java
@@ -0,0 +1,81 @@
+package chapter06;
+
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+
+public class Window_IncreAggr {
+    public static void main(String[] args) throws Exception {
+        // get the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
+        env.setParallelism(1);
+
+
+        // get input data by connecting to the socket
+        DataStream<String> text = env.socketTextStream("192.168.40.101", 9999, "\n");
+
+        // parse the data, group it, window it, and aggregate the counts
+        KeyedStream<Tuple2<String, Integer>, String> keyedStream = text.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                // normalize and split the line
+                String[] tokens = value.toLowerCase().split("\\W+");
+
+                // emit the pairs
+                for (String token : tokens) {
+                    if (token.length() > 0) {
+                        out.collect(new Tuple2<>(token, 1));
+                    }
+                }
+            }
+        }).keyBy(value -> value.f0);
+
+        WindowedStream<Tuple2<String, Integer>, String, TimeWindow> dataWS = keyedStream.timeWindow(Time.seconds(10));
+
+        DataStream<Integer> aggregateDs = dataWS.aggregate(new AggregateFunction<Tuple2<String, Integer>, Integer, Integer>() {
+            //初始化  => 初始值 0
+            //注意:这个不是open方法,不是只初始化一次
+            @Override
+            public Integer createAccumulator() {
+                return 0;
+            }
+
+            //累加操作 => 每来一条数据，如何进行累加
+            @Override
+            public Integer add(Tuple2<String, Integer> value, Integer accumulator) {
+                System.out.println("add...");
+                return accumulator + 1;
+            }
+
+            //获取累加器结果
+            @Override
+            public Integer getResult(Integer accumulator) {
+                System.out.println("get result...");
+                return accumulator;
+            }
+
+            //合并多个累加器的结果，会话窗口才会调用
+            //debug调试的时候确实不会执行这里
+            @Override
+            public Integer merge(Integer a, Integer b) {
+                return a + b;
+            }
+        });
+
+        aggregateDs.print("------------->聚合结果:----------->");
+
+        env.execute("Window_IncreAggr");
+    }
+}
