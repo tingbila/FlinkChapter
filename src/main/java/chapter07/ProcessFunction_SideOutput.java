e26a3d14ad45af9421ffe7b203bc1a6de09250ef
ajjsk <ajjsk@qq.com>
2023-04-09 21:48:05 +0800

    侧输出流

---
+++ src/main/java/chapter07/ProcessFunction_SideOutput.java
@@ -0,0 +1,68 @@
+package chapter07;
+
+
+import bean.WaterSensor;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.RestOptions;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+import java.sql.Timestamp;
+
+// 案例：采集监控传感器水位值，将水位值高于100的值输出到side output。
+public class ProcessFunction_SideOutput {
+    public static void main(String[] args) throws Exception {
+        // get the execution environment
+        Configuration conf = new Configuration();
+        conf.set(RestOptions.PORT, 8083);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
+        env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        // get input data by connecting to the socket
+        DataStream<String> dataStreamSource = env.socketTextStream("192.168.40.101", 9999, "\n");
+        DataStream<WaterSensor> mapDataStream = dataStreamSource.map(new RichMapFunction<String, WaterSensor>() {
+            @Override
+            public WaterSensor map(String line) throws Exception {
+                String[] spited = line.split(",");
+                return new WaterSensor(spited[0], Long.valueOf(spited[1]), Integer.valueOf(spited[2]));
+            }
+        }).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<WaterSensor>(Time.seconds(2)) {  //数据最大乱序时间
+            @Override
+            public long extractTimestamp(WaterSensor element) {   //watermark = max(event_time) - maxOutOfOrderness 看源代码
+                return element.getTs() * 1000; //指定事件时间
+            }
+        });
+
+        mapDataStream.print();
+        KeyedStream<WaterSensor, String> keyedStream = mapDataStream.keyBy(value -> value.getId());
+
+        OutputTag<String> outputTag = new OutputTag<String>("WaterSensor Alarm") {};
+        SingleOutputStreamOperator<WaterSensor> processDataStream = keyedStream.process(new KeyedProcessFunction<String, WaterSensor, WaterSensor>() {
+            @Override
+            public void processElement(WaterSensor value, Context ctx, Collector<WaterSensor> out) throws Exception {
+                if (value.getVc() > 100) {
+                    ctx.output(outputTag, value.toString() + "水位高于阈值5,请关注！！！");  //侧输出流的元素可以自己定义!!!
+                } else {
+                    out.collect(value);
+                }
+            }
+        });
+
+        processDataStream.print();
+        DataStream<String> sideOutput = processDataStream.getSideOutput(outputTag);
+        sideOutput.print("alarm");
+
+        env.execute("ProcessFunction_SideOutput");
+    }
+}
