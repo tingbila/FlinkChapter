12039e81fb8327805fe57453ac77d59dbf726f80
ajjsk <ajjsk@qq.com>
2023-04-09 18:43:39 +0800

    查看flink状态变量的源代码

--- src/main/java/chapter07/WaterSensor_Alert.java
+++ src/main/java/chapter07/WaterSensor_Alert.java
@@ -20,6 +20,7 @@ import java.sql.Timestamp;
 
 //需求：监控温度传感器的温度值，如果传感器温度值在5秒钟之内(event time)连续上升，则报警
 //watermark和分组没有关系
+//深究的话程序还有很多问题!!!!!
 public class WaterSensor_Alert {
     public static void main(String[] args) throws Exception {
         // get the execution environment
@@ -44,13 +45,13 @@ public class WaterSensor_Alert {
             }
         });
 
-        mapDataStream.print();
+        //mapDataStream.print();
         KeyedStream<WaterSensor, String> keyedStream = mapDataStream.keyBy(value -> value.getId());
         DataStream<String> processDataStream = keyedStream.process(new KeyedProcessFunction<String, WaterSensor, String>() {
             //定义一个变量，保存上一次的水位值.
             private Integer lastWaterSensorValue = Integer.MIN_VALUE;
             //定义一个变量，用来记录是否已经注册过定时器.
-            private Integer isRegister = 0;
+            private Long isRegister = 0L;
 
             //来一条数据，处理一条数据，类比MR当中Mapper当中的map方法.
             //只有当算子的watermark的时间大于定时器的时间，才会触发定时器。
@@ -60,7 +61,7 @@ public class WaterSensor_Alert {
                 if (isRegister == 0) { //如果还没有注册定时器
                     ctx.timerService().registerEventTimeTimer(value.getTs() * 1000L + 5000L);
                     System.out.println("注册定时器时间是: " + new Timestamp(value.getTs() * 1000L));
-                    isRegister = value.getTs().intValue() * 1000 + 5000;
+                    isRegister = value.getTs().intValue() * 1000L + 5000L;
                 } else {
                     if (value.getVc() <= lastWaterSensorValue) {
                         //删除之前注册的定时器
@@ -68,7 +69,7 @@ public class WaterSensor_Alert {
                         //重新注册定时器
                         ctx.timerService().registerEventTimeTimer(value.getTs() * 1000L + 5000L);
                         System.out.println("重新注册定时器的时间是: " + new Timestamp(value.getTs() * 1000L));
-                        isRegister = value.getTs().intValue() * 1000 + 5000;
+                        isRegister = value.getTs().intValue() * 1000L + 5000L;
                     }
                 }
                 //不管上升还是下降，都要保存水位值，供下条数据使用，进行比较
@@ -87,7 +88,7 @@ public class WaterSensor_Alert {
                 //定时器触发，说明已经满足连续5秒水位上升
                 System.out.println("主人,主人,主人...监测到水位连续5s上升,当前时间是:" + new Timestamp(timestamp) + "触发的watermark时间是: " + ctx.timerService().currentWatermark());
                 //定时器触发完之后恢复初始化状态
-                isRegister = 0;
+                isRegister = 0L;
             }
         });
 
