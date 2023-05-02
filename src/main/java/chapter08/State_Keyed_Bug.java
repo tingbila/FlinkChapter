9dc6e1534bed8fbfc1b0ec2b9adb15c92c473ece
ajjsk <ajjsk@qq.com>
2023-05-02 18:30:20 +0800

    状态隔离

---
+++ src/main/java/chapter08/State_Keyed_Bug.java
@@ -0,0 +1,57 @@
+package chapter08;
+
+import bean.WaterSensor;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import java.sql.Timestamp;
+
+
+//keyed-state之间状态不隔离
+
+
+public class State_Keyed_Bug {
+    public static void main(String[] args) throws Exception {
+        // get the execution environment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        // get input data by connecting to the socket
+        DataStream<String> dataStreamSource = env.socketTextStream("192.168.40.101", 9999, "\n");
+        DataStream<WaterSensor> mapDataStream = dataStreamSource.map(new RichMapFunction<String, WaterSensor>() {
+            @Override
+            public WaterSensor map(String line) throws Exception {
+                String[] spited = line.split(",");
+                return new WaterSensor(spited[0], Long.valueOf(spited[1]), Integer.valueOf(spited[2]));
+            }
+        }).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<WaterSensor>(Time.seconds(2)) {  //数据最大乱序时间
+            @Override
+            public long extractTimestamp(WaterSensor element) {   //watermark = max(event_time) - maxOutOfOrderness 看源代码
+                return element.getTs() * 1000; //指定事件时间
+            }
+        });
+
+        mapDataStream.print();
+        KeyedStream<WaterSensor, String> keyedStream = mapDataStream.keyBy(value -> value.getId());
+        DataStream<String> processDataStream = keyedStream.process(new KeyedProcessFunction<String, WaterSensor, String>() {
+            private Integer lastVc = 0;  //此时lastVc多个key所共享
+
+            @Override
+            public void processElement(WaterSensor value, Context ctx, Collector<String> out) throws Exception {
+                out.collect("当前数据所属的key=" + ctx.getCurrentKey() + ",保存的上一次水位值是=" + lastVc);
+                lastVc = value.getVc();
+            }
+        });
+
+        processDataStream.print();
+        env.execute("State_Keyed_Bug");
+    }
+}
