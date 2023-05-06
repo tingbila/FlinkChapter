3861cd1717f8dd09b99024d652d7b64899734e23
ajjsk <ajjsk@qq.com>
2023-05-06 22:13:45 +0800

    init

--- src/main/java/chapter07/KeyedProcessFunctionOnlyOneTimer.java
+++ src/main/java/chapter07/KeyedProcessFunctionOnlyOneTimer.java
@@ -63,16 +63,9 @@ public class KeyedProcessFunctionOnlyOneTimer {
                 }
             }
 
-            /**
-             * 到了定时的时间要干什么事情
-             * @param timestamp   定时器设置的时间,可以理解为闹钟响的时间.
-             * @param ctx
-             * @param out
-             * @throws Exception
-             */
             @Override
             public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
-                System.out.println("主人,主人,主人...快起床了,当前时间是:" + new Timestamp(timestamp));
+                System.out.println("主人,主人,主人...闹钟响了,当前时间是:" + new Timestamp(timestamp));
                 triggerTs = 0L;
             }
         });
