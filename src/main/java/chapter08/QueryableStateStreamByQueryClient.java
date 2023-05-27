66948aa7e05d5fdf76b5bf1d2ceccfbe930568b4
ajjsk <ajjsk@qq.com>
2023-05-27 20:01:50 +0800

    如何进行状态查询

--- src/main/java/chapter08/QueryableStateStreamByQueryClient.java
+++ src/main/java/chapter08/QueryableStateStreamByQueryClient.java
@@ -15,15 +15,14 @@ import org.apache.flink.queryablestate.client.QueryableStateClient;
 //方式1:通过Queryable State Stream使状态可查
 public class QueryableStateStreamByQueryClient {
     public static void main(String[] args) throws Exception {
-        //1. 提供任意一个TaskManager的主机名以及可查询式状态的代理的监听端口,客户端代理的默认监听端口是9067
-        QueryableStateClient client = new QueryableStateClient("localhost", 9069);
+        //1. 提供任意一个TaskManager的主机名以及可查询式状态的代理监听端口,默认监听端口是9067
+        QueryableStateClient client = new QueryableStateClient("localhost", 9067);
 
         String queryKey = "hello";
         ValueStateDescriptor<Tuple2<String, Integer>> stateDescriptor =
                 new ValueStateDescriptor<>(
                         "query-name",
-                        TypeInformation.of(new TypeHint<Tuple2<String, Integer>>() {
-                        }));  //状态的类型
+                        TypeInformation.of(new TypeHint<Tuple2<String, Integer>>() {}));  //状态的类型
 
         //2. 通过client.getKvState方法来获取具体key对应的value状态信息
         while (true) {
@@ -31,7 +30,7 @@ public class QueryableStateStreamByQueryClient {
                     JobID.fromHexString("707fbba47bdc81404523b70d14001831"),
                     "query-name",
                     queryKey,
-                    BasicTypeInfo.STRING_TYPE_INFO,
+                    BasicTypeInfo.STRING_TYPE_INFO,   //key的类型
                     stateDescriptor);
 
             System.out.println(resultFuture.get().value());
