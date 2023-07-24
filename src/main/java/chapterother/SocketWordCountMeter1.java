4cc557792f362cdba7d4bdf099b5e8d046a5a519
ajjsk <ajjsk@qq.com>
2023-07-24 20:43:24 +0800

    init

---
+++ src/main/java/chapterother/SocketWordCountMeter1.java
@@ -0,0 +1,63 @@
+package chapterother;
+
+import bean.WaterSensor;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.RestOptions;
+import org.apache.flink.metrics.Histogram;
+import org.apache.flink.metrics.Meter;
+import org.apache.flink.metrics.MeterView;
+import org.apache.flink.metrics.MetricGroup;
+import org.apache.flink.runtime.metrics.DescriptiveStatisticsHistogram;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+
+//Flink流处理案例开发:Histogram直方图数据分布:Flink中代码提供了内置实现描述性统计直方图
+public class SocketWordCountMeter1 {
+    public static void main(String[] args) throws Exception {
+        // get the execution environment
+        Configuration conf = new Configuration();
+        conf.set(RestOptions.PORT, 8083);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
+        env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        // get input data by connecting to the socket
+        DataStream<String> dataStreamSource = env.socketTextStream("192.168.40.101", 9999, "\n");
+        DataStream<WaterSensor> mapDataStream = dataStreamSource.map(new RichMapFunction<String, WaterSensor>() {
+            @Override
+            public WaterSensor map(String line) throws Exception {
+                String[] spited = line.split(",");
+                return new WaterSensor(spited[0], Long.valueOf(spited[1]), Integer.valueOf(spited[2]));
+            }
+        }).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<WaterSensor>(Time.seconds(2)) {  //数据最大乱序时间
+            @Override
+            public long extractTimestamp(WaterSensor element) {   //watermark = max(event_time) - maxOutOfOrderness 看源代码
+                return element.getTs() * 1000; //指定事件时间
+            }
+        });
+
+        DataStream<WaterSensor> streamOperator = mapDataStream.map(new RichMapFunction<WaterSensor, WaterSensor>() {
+            private transient Meter meter;
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                MetricGroup metricGroup = getRuntimeContext().getMetricGroup();
+                MetricGroup myGroup = metricGroup.addGroup("MyGroup");  //counter组
+                meter = myGroup.meter("myMeter", new MeterView(60));   //Flink中代码提供了内置实现MeterView
+            }
+            @Override
+            public WaterSensor map(WaterSensor value) throws Exception {
+                this.meter.markEvent();
+                return value;
+            }
+        });
+
+        streamOperator.print();
+
+        env.execute("SocketWordCountMeter1");
+    }
+}
